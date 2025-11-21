2850af4912d41f4221d7e773371cbb7873435008
David Lazic <lazic.ddavid@gmail.com>
2025-11-21 10:26:55 +0100

    Added const cards [   {id:1, name:, isClicked: false, }   {id:2, name:, isClicked: false, }   {id:3, name:, isClicked: false, }   {id:4, name:, isClicked: false, }   {id:5, name:, isClicked: false, }
    ];

---
+++ memgames.js
@@ -0,0 +1,10 @@
+const cards [
+  {id:1, name:, isClicked: false, }
+  {id:2, name:, isClicked: false, }
+  {id:3, name:, isClicked: false, }
+  {id:4, name:, isClicked: false, }
+  {id:5, name:, isClicked: false, }
+
+];
+
+let currentScore = 0;

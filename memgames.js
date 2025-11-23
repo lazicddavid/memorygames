af2e53d816565b2da07c31dab5bbf562ca20dbcb
David Lazic <lazic.ddavid@gmail.com>
2025-11-23 22:54:11 +0100

    Added const cards = [   { id: 1, isClicked: false },   { id: 2, isClicked: false },   { id: 3, isClicked: false },   { id: 4, isClicked: false },   { id: 5, isClicked: false },   { id: 6, isClicked: false },   { id: 7, isClicked: false },   { id: 8, isClicked: false },   { id: 9, isClicked: false },   { id: 10, isClicked: false },   { id: 11, isClicked: false },   { id: 12, isClicked: false }, ];

--- memgames.js
+++ memgames.js
@@ -62,3 +62,18 @@ const cardManager = {
     return this.cards;
   },
 };
+
+const cards = [
+  { id: 1, isClicked: false },
+  { id: 2, isClicked: false },
+  { id: 3, isClicked: false },
+  { id: 4, isClicked: false },
+  { id: 5, isClicked: false },
+  { id: 6, isClicked: false },
+  { id: 7, isClicked: false },
+  { id: 8, isClicked: false },
+  { id: 9, isClicked: false },
+  { id: 10, isClicked: false },
+  { id: 11, isClicked: false },
+  { id: 12, isClicked: false },
+];
